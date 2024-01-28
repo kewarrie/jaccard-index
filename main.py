379b363d0201149f2ea8dc48be041173355ae419
Kevin <65783003+kewarrie@users.noreply.github.com>
2024-01-28 20:37:48 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+'''
+Jaccard Similarity Coefficient, also known as Jaccard index,
+is a statistic to measure the similarity between two data sets.
+
+The Jaccard coefficient measures similarity between finite sample
+sets, and is defined as the size of the intersection divided by
+the size of the union of the sample sets:
+
+            |AnB|
+J(A,B) = -----------
+            |AUB|
+
+By design: 0 <= J(A,B) <= 1
+
+Source: https://www.geeksforgeeks.org/how-to-calculate-jaccard-similarity-in-python/
+'''     
+
+A = {1, 2, 3, 4, 6}
+B = {1, 2, 5, 8, 9}
+
+# Intersaction and Union of two sets can also be done using & and | operators.
+C = A.intersection(B)
+D = A.union(B)
+J = float(len(C))/float(len(D))
+
+print('AnB = ', C)
+print('AUB = ', D)
+print('J(A,B) = ', J)
